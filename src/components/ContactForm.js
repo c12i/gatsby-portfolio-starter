080a0f6793308031ee47f6f2d4c56df84f0eca3d
Collins Muriuki <murerwacollins@gmail.com>
2020-09-22 22:03:41 +0300

    Create layout and basic components

---
+++ src/components/ContactForm.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ContactForm = () => {
+  return <div>hi from contact form</div>
+}
+
+export default ContactForm
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Footer = () => {
+  return <div>hi from footer</div>
+}
+
+export default Footer
