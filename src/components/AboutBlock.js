6c6c430e3757755a78ea719b381867f33005f001
Collins Muriuki <murerwacollins@gmail.com>
2020-09-23 09:49:18 +0300

    Update About* component nomenclature

--- src/components/AboutBlock.js
+++ src/components/AboutBlock.js
@@ -1,8 +1,17 @@
 import React from "react"
 
 import AboutHero from "../components/AboutHero"
-import InfoCard from "../components/InfoCard"
-import AboutMeData from "../components/AboutMeData"
+import AboutInfo from "./AboutInfo"
+import AboutBio from "./AboutBio"
+
+const dummyText = `Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi
+umami everyday carry hexagon locavore direct trade art party. Locavore
+small batch listicle gastropub farm-to-table lumbersexual salvia
+messenger bag. Coloring book flannel truffaut craft beer drinking
+vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland
+90's scenester vexillologist forage post-ironic asymmetrical, chartreuse
+disrupt butcher paleo intelligentsia pabst before they sold out four
+loko. 3 wolf moon brooklyn.`
 
 const AboutBlock = ({ hideHero }) => {
   return (
@@ -11,8 +20,12 @@ const AboutBlock = ({ hideHero }) => {
         <div className="lg:w-4/6 mx-auto">
           {!hideHero && <AboutHero />}
           <div className="flex flex-col sm:flex-row mt-10">
-            <InfoCard />
-            <AboutMeData />
+            <AboutInfo
+              name="Collins Muriuki"
+              title="Software Engineer"
+              company="TIBU"
+            />
+            <AboutBio bio={dummyText} />
           </div>
         </div>
       </div>
--- src/components/AboutInfo.js
+++ src/components/AboutInfo.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const InfoCard = () => {
+const InfoCard = ({ name, title, company }) => {
   return (
     <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
       <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
@@ -19,11 +19,11 @@ const InfoCard = () => {
       </div>
       <div className="flex flex-col items-center text-center justify-center">
         <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
-          Collins Muriuki
+          {name}
         </h2>
         <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
         <p className="text-base px-2 text-gray-600">
-          Jr. Software Engineer at TIBU Health
+          {title} at {company}
         </p>
       </div>
     </div>
