758c64abcc193ac5cc18c881f88f12cd27144f18
Collins Muriuki <murerwacollins@gmail.com>
2020-09-25 15:16:19 +0300

    Implement AniLink on 404 page component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link, useStaticQuery } from "gatsby";
+import { useStaticQuery } from "gatsby";
 import { FaQuoteRight } from "react-icons/fa"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 import Layout from "../components/Layout"
 
@@ -23,7 +24,12 @@ const Four04 = () => {
   const { author, content, source } = data.site.siteMetadata.quote
   return (
     <Layout hideNav hideFooter>
-      <Link to="/" className="text-gray-700 body-font">
+      <AniLink
+        swipe
+        direction="down"
+        to="/"
+        className="text-gray-700"
+      >
         <div className="container px-5 py-24 mx-auto">
           <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
             <FaQuoteRight className="inline-block w-8 h-8 text-gray-400 mb-8" />
@@ -35,7 +41,7 @@ const Four04 = () => {
             <p className="text-gray-500 py-5">{source}</p>
           </div>
         </div>
-      </Link>
+      </AniLink>
     </Layout>
   )
 }
