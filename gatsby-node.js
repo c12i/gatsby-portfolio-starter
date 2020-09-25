14b91c53580596e9d8a8abe39fd1e7a56da3d384
Collins Muriuki <murerwacollins@gmail.com>
2020-09-26 00:24:29 +0300

    Create blog-template and programatically generate blogs. Also install ReactMarkdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,24 @@
-exports.createPages = async ({ graphql, actions }) => {};
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const { data } = await graphql(`
+    {
+      allStrapiBlogs {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  data.allStrapiBlogs.edges.forEach(({ node: { slug } }) => {
+    createPage({
+      path: `blog/${slug}`,
+      component: path.resolve(`./src/templates/blog-template.js`),
+      context: { slug },
+    })
+  })
+}
---
+++ src/templates/blog-template.js
@@ -0,0 +1,113 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import ReactMarkdown from "react-markdown"
+
+import SEO from "../components/SEO"
+import Layout from "../components/Layout"
+import SmallHero from "../components/SmallHero"
+
+const BlogTemplate = ({ data: { blog, bio } }) => {
+  const {
+    title,
+    category,
+    date,
+    excerpt,
+    content,
+    image: {
+      childImageSharp: { fluid: blogImg },
+    },
+  } = blog
+  const {
+    name,
+    photo: {
+      childImageSharp: { fluid: author },
+    },
+  } = bio
+  return (
+    <Layout>
+      <SEO title={title} description={excerpt} />
+      <section className="container w-full md:max-w-3xl mx-auto md:pt-20">
+        <div className="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal">
+          <SmallHero img={blogImg} />
+          <h1 className="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
+            {title}
+          </h1>
+          <p className="text-sm md:text-base font-normal text-gray-600">
+            {date}
+          </p>
+          <article className="py-6">
+            <ReactMarkdown source={content} />
+          </article>
+        </div>
+
+        <div className="text-base md:text-sm text-gray-500 px-4 py-6">
+          Category:
+          <span
+            href="#"
+            className="text-base md:text-sm text-indigo-500 no-underline"
+          >
+            {category}
+          </span>
+        </div>
+        <hr className="border-b-2 border-gray-400 mb-8 mx-4" />
+
+        <div className="flex w-full items-center text-sm px-4 py-12">
+          <Img
+            className="w-10 h-10 rounded-full mr-4"
+            fluid={author}
+            alt="Avatar of Author"
+          />
+          <div className="flex-1 px-2">
+            <p className="text-base font-bold text-base md:text-xl leading-none mb-2">
+              {name}
+            </p>
+          </div>
+          <div className="justify-end">
+            <AniLink
+              swipe
+              direction="right"
+              to="/blog"
+              className="bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-300 font-bold py-2 px-4 rounded-full"
+            >
+              Read More
+            </AniLink>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      title
+      category
+      date(formatString: "MMMM Do, YYYY")
+      content
+      excerpt
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+
+    bio: strapiBio {
+      name
+      photo {
+        childImageSharp {
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  }
+`
+
+export default BlogTemplate
