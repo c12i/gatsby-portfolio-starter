d54ad30cf0d801482bd56d314f8027f89ce43510
Collins Muriuki <murerwacollins@gmail.com>
2020-09-27 00:14:51 +0300

    Update formspree.io form meta-data

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -39,6 +39,7 @@ const ContactForm = () => {
                 className="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
                 placeholder="Name"
                 type="text"
+                name="name"
                 required
               />
             </div>
